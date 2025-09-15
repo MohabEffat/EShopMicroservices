227d5045cffb5b473ba96937d2cc7427649f6328
MohabEffat <99203754+MohabEffat@users.noreply.github.com>
2025-09-15 23:31:52 +0300

    Finished Domain Layer

---
+++ eshop-microservice/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
@@ -0,0 +1,16 @@
+﻿namespace Ordering.Domain.ValueObjects
+{
+    public record CustomerId
+    {
+        public Guid Value { get; }
+        private CustomerId(Guid value) => Value = value;
+
+        public static CustomerId Of(Guid value)
+        {
+            if (value == Guid.Empty)
+                throw new DomainException("Customer Id can't be empty.");
+
+            return new CustomerId(value);
+        }
+    }
+}
---
+++ eshop-microservice/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -0,0 +1,24 @@
+﻿namespace Ordering.Domain.ValueObjects
+{
+    public record OrderName
+    {
+        private const int RequiredLength = 5;
+        public string Value { get; }
+
+        private OrderName(string value) => Value = value;
+
+        public static OrderName Of(string value)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(value);
+
+            if (value.Length != RequiredLength)
+                throw new ArgumentOutOfRangeException(nameof(value),
+                    $"Order name must be exactly {RequiredLength} characters long.");
+
+            return new OrderName(value);
+        }
+
+        public override string ToString() => Value;
+    }
+
+}
